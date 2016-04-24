8cb896b590145781fd0d8bdf8aa011f6ccfc41a0
binarios01 <i.igoumenakis@gmail.com>
2016-04-24 06:17:43 +0300

    index commit

---
+++ fintech.Web/Scripts/app/app.js
@@ -0,0 +1,47 @@
+﻿var app = angular.module("app", ["ngRoute"])
+.config(["$routeProvider", function ($routeProvider) {
+    $routeProvider.
+        when('/', {
+            templateUrl: '../scripts/app/index/index.html',
+            controller: 'indexCtrl'
+        }).
+        when('/poll/:id', {
+            templateUrl: '../scripts/app/poll/poll.html',
+            controller: 'pollCtrl'
+        }).
+        when('/needs', {
+            templateUrl: '../scripts/app/needs/needs.html',
+            controller: 'needsCtrl'
+        }).
+        when('/referrals', {
+            templateUrl: '../scripts/app/referrals/referrals.html',
+            controller: 'referralsCtrl'
+        }).
+        when('/brands', {
+            templateUrl: '../scripts/app/brands/brands.html',
+            controller: 'brandsCtrl'
+        }).
+        when('/wishlist', {
+            templateUrl: '../scripts/app/wishlist/wishlist.html',
+            controller: 'wishlistCtrl'
+        }).when('/startPoll', {
+            templateUrl: '../scripts/app/startPoll/startPoll.html',
+            controller: 'startPollCtrl'
+        }).when('/explore', {
+            templateUrl: '../scripts/app/explore/explore.html',
+            controller: 'exploreCtrl'
+        }).when('/camera', {
+            templateUrl: '../scripts/app/camera/camera.html',
+            controller: 'cameraCtrl'
+        }).when('/tapAndPlay', {
+            templateUrl: '../scripts/app/tapAndPlay/tapAndPlay.html',
+            controller: 'tapAndPlayCtrl'
+        }).when('/profile', {
+            templateUrl: '../scripts/app/profile/profile.html',
+            controller: 'profileCtrl'
+        }).
+
+    otherwise({
+        redirectTo: '/'
+    });
+}]);
---
+++ fintech.Web/Scripts/app/index/services/pollDataHttp.js
@@ -0,0 +1,8 @@
+﻿app.service("pollDataHttp", ["$http", function ($http) {
+
+    return {
+        getMyFriendsPolls: function (req) {
+            return $http.post("api/referraApi/getMyFriendsPolls", req);
+        }
+    };
+}])
