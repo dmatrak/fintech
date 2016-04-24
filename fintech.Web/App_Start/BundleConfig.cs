eaca37ed04c3772a388b420e0aebf3a074a71230
jon <i.igoumenakis@gmail.com>
2016-04-24 12:42:02 +0300

    fix

--- fintech.Web/App_Start/BundleConfig.cs
+++ fintech.Web/App_Start/BundleConfig.cs
@@ -37,7 +37,7 @@ namespace fintech.Web
                 .Include("~/Scripts/app/index/directives/poll.js")
                 .Include("~/Scripts/app/needs/needsCtrl.js")
                 .Include("~/Scripts/app/needs/needDetailsCtrl.js")
-                .Include("~/Scripts/app/referral/referralListCtrl.js")
+                .Include("~/Scripts/app/referral/referralList.js")
                 );
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
